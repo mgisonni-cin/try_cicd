28bef98cc9cfec5c9571ba681a2a5d362598fe0f
mgisonni <m.gisonni@cineca.it>
2025-03-25 11:57:31 +0100

    m

--- build_container.sh
+++ build_container.sh
@@ -4,7 +4,7 @@
 CONTAINER_NAME="my_container.sif"
 
 # Build the Singularity container with --fakeroot option
-singularity build --remote $CONTAINER_NAME Singularity.def
+sudo singularity build $CONTAINER_NAME Singularity.def
 
 echo "Container built successfully!"
 
