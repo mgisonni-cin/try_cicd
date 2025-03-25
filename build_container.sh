a6f2fdd700420ea904830b2518d66c435a325841
mgisonni <m.gisonni@cineca.it>
2025-03-25 11:35:04 +0100

    Initial commit with CI/CD pipeline

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "Building Singularity container..."
-singularity build my_container.sif Singularity.def
+
+# Define the container image name
+CONTAINER_NAME="my_container.sif"
+
+# Build the Singularity container with --fakeroot option
+singularity build --fakeroot $CONTAINER_NAME Singularity.def
+
+echo "Container built successfully!"
 
